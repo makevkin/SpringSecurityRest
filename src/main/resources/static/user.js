7b19f9cf3f51a61a40f7e296bfba17132525c5f9
Юрий Макевкин <makevkin@mail.ru>
2024-03-03 17:30:17 +0400

    admin panel

--- src/main/resources/static/user.js
+++ src/main/resources/static/user.js
@@ -1,22 +1,32 @@
-$(async function () {
-    await loadUser();
-});
+// $(async function () {
+//     await loadUser();
+// });
+//
+// async function loadUser() {
+//     fetch("http://localhost:8088/api/user")
+//         .then(r => r.json())
+//         .then(data => {
+//             $('#navUsername').append(data.username);
+//             let roles = data.roles.map(role => " " + role.name.substring(5));
+//             $('#navRoles').append(roles);
+//             let user = `$(
+//             <tr>
+//                 <td>${data.id}</td>
+//                 <td>${data.username}</td>
+//                 <td>${roles}</td>)`;
+//             $('#userPanelBody').append(user);
+//         })
+//         .catch((error) => {
+//             alert(error);
+//         });
+// }
 
-async function loadUser() {
-    fetch("http://localhost:8088/api/user")
-        .then(r => r.json())
-        .then(data => {
-            $('#navUsername').append(data.username);
-            let roles = data.roles.map(role => " " + role.name.substring(5));
-            $('#navRoles').append(roles);
-            let user = `$(
-            <tr>
-                <td>${data.id}</td>
-                <td>${data.username}</td>
-                <td>${roles}</td>)`;
-            $('#userPanelBody').append(user);
+
+    fetch("http://localhost:8088/api/admin/1")
+        .then(response => response.json())
+        .then(data => {console.log(data)
+             let p = document.getElementById('userrr')
+             p.innerHTML =data.username
         })
-        .catch((error) => {
-            alert(error);
-        });
-}
+
+
